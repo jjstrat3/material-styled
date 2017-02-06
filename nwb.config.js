a5e9f3b7034f586bae7630d4daafca98d19564cd
Jay Stratton <jjstrat3@gmail.com>
2017-02-06 13:01:34 -0600

    Linting and misc. config with dot files.  Tweaked `nwb.config.js` for stage-1 babel support and enzyme compatibility.

--- nwb.config.js
+++ nwb.config.js
@@ -1,5 +1,15 @@
+var isTest = process.env.NODE_ENV === 'test'
+
 module.exports = {
   type: 'react-component',
+  babel: {
+    stage: 1
+  },
+  webpack: {
+    compat: {
+      enzyme: isTest ? true : false
+    }
+  },
   npm: {
     esModules: true,
     umd: {
